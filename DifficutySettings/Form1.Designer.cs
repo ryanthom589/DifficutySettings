a4cb95cdba19176218806afb94b2e8ddb725ada4
ryanthom589 <ryanthom589@ed.amdsb.ca>
2019-10-01 13:23:46 -0400

    Add project files.

---
+++ DifficutySettings/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace DifficutySettings
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mediumButton = new System.Windows.Forms.Button();
+            this.hardButton = new System.Windows.Forms.Button();
+            this.easyButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.heroNameInput = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // mediumButton
+            // 
+            this.mediumButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.mediumButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.mediumButton.Font = new System.Drawing.Font("MV Boli", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mediumButton.ForeColor = System.Drawing.Color.White;
+            this.mediumButton.Location = new System.Drawing.Point(345, 236);
+            this.mediumButton.Name = "mediumButton";
+            this.mediumButton.Size = new System.Drawing.Size(129, 56);
+            this.mediumButton.TabIndex = 0;
+            this.mediumButton.Text = "Medium";
+            this.mediumButton.UseVisualStyleBackColor = true;
+            this.mediumButton.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // hardButton
+            // 
+            this.hardButton.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.hardButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Red;
+            this.hardButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.hardButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.hardButton.Font = new System.Drawing.Font("MV Boli", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.hardButton.ForeColor = System.Drawing.Color.White;
+            this.hardButton.Location = new System.Drawing.Point(512, 234);
+            this.hardButton.Name = "hardButton";
+            this.hardButton.Size = new System.Drawing.Size(124, 60);
+            this.hardButton.TabIndex = 1;
+            this.hardButton.Text = "Hard";
+            this.hardButton.UseVisualStyleBackColor = true;
+            this.hardButton.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // easyButton
+            // 
+            this.easyButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Lime;
+            this.easyButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.easyButton.Font = new System.Drawing.Font("MV Boli", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.easyButton.ForeColor = System.Drawing.Color.White;
+            this.easyButton.Location = new System.Drawing.Point(182, 236);
+            this.easyButton.Name = "easyButton";
+            this.easyButton.Size = new System.Drawing.Size(122, 56);
+            this.easyButton.TabIndex = 2;
+            this.easyButton.Text = "Easy";
+            this.easyButton.UseVisualStyleBackColor = true;
+            this.easyButton.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(182, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Welcome Player";
+            // 
+            // heroNameInput
+            // 
+            this.heroNameInput.Location = new System.Drawing.Point(345, 47);
+            this.heroNameInput.Name = "heroNameInput";
+            this.heroNameInput.Size = new System.Drawing.Size(217, 22);
+            this.heroNameInput.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MV Boli", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(179, 170);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(222, 31);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Choose Difficulty";
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Segoe Print", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(345, 359);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(129, 56);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Begin";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.heroNameInput);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.easyButton);
+            this.Controls.Add(this.hardButton);
+            this.Controls.Add(this.mediumButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button mediumButton;
+        private System.Windows.Forms.Button hardButton;
+        private System.Windows.Forms.Button easyButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox heroNameInput;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ DifficutySettings/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DifficutySettings
+{
+    public partial class Form1 : Form
+    {
+        String heroName;
+        String difficultyLevel;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            //Easy Button
+            difficultyLevel = "an easy game";
+            easyButton.BackColor = Color.Lime;
+            mediumButton.BackColor = Color.Black;
+            hardButton.BackColor = Color.Black;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button1_Click_1(object sender, EventArgs e)
+        {
+            //Begin Button
+            heroName = heroNameInput.Text;
+            label2.Text = heroName + ", you will have " + difficultyLevel;
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            //Medium Button
+            difficultyLevel = "a fair game";
+            mediumButton.BackColor = Color.Orange;
+            easyButton.BackColor = Color.Black;
+            hardButton.BackColor = Color.Black;
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            //Hard Button
+            difficultyLevel = "a hard game";
+            hardButton.BackColor = Color.Red;
+            mediumButton.BackColor = Color.Black;
+            easyButton.BackColor = Color.Black;
+          
+        }
+    }
+}
